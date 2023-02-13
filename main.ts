3d7733f47bfe553eb14c4745a040d7afa41b627e
Jeet-Lad <123616298+Jeet-Lad@users.noreply.github.com>
2023-02-13 10:33:36 -0800

    Redone

--- main.ts
+++ main.ts
@@ -1,35 +1,69 @@
 let Hunger = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        # # # . .
+        . . . . .
+        `)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # . . . #
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . # # #
+        . . . . .
+        `)
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
-        . # . # .
-        . # . # .
         . . . . .
-        . # # # .
-        . # . # .
+        # # # # #
+        # . . . #
+        # . . . #
+        . . . . .
         `)
     music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 225)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
-})
-basic.forever(function () {
-    basic.showString("Meimu!")
-    music.playMelody("E D G F B A C5 B ", 120)
-    Hunger = 5
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
 })
 basic.forever(function () {
     if (Hunger == 0) {
         for (let index = 0; index < 4; index++) {
             music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 225)
         }
+        led.setBrightness(0)
     }
+    if (input.temperature() < 16) {
+        basic.showString("Brr!")
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . . . #
+        # # # # #
+        . . . . .
+        `)
 })
 loops.everyInterval(3600000, function () {
     Hunger += Hunger - 1
